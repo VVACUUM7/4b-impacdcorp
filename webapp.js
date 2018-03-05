b1acbbac0bef328c598215faeb27633ba5738ce3
Aditya Pawar <aditya.pawar@warriorlife.net>
2018-03-05 15:05:36 -0800

    Gold Moles

--- webapp.js
+++ webapp.js
@@ -5,17 +5,126 @@ var downloadTimer;
 var inter;
 var inter1;
 var inter5;
+var inter10;
 var death;
 var timeleft;
 var saveScore;
 var array;
+var coord1;
 var highScore = 0;
 var coords;
 var total = 0;
 
 
-// Random Function
+function addScore11(){
+				currentScore++;
+				document.getElementById("score").innerHTML = currentScore;
+				array = document.getElementsByClassName("active1");
+		for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore11);
+		array[i].setAttribute("class", "void");
+		
+	}
+			}
+			
+function addScore1(){
+				currentScore++;
+				document.getElementById("score").innerHTML = currentScore;
+				array = document.getElementsByClassName("active");
+		for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore1);
+		array[i].setAttribute("class", "void");
+		
+	}
+			}
+			
+	function addScore5(){
+				currentScore = currentScore + 5;
+		
+				document.getElementById("score").innerHTML = currentScore;
+				array = document.getElementsByClassName("silver");
+		for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore5);
+		array[i].setAttribute("class", "void");
+			}
 
+}
+
+function  addScore10() {
+	
+				currentScore = currentScore + 10;
+		
+				document.getElementById("score").innerHTML = currentScore;
+				array = document.getElementsByClassName("gold");
+		for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore10);
+		array[i].setAttribute("class", "void");
+		}
+	}
+//picks random coord for mole
+
+function removeMoles5() {
+	
+
+	var array = document.getElementsByClassName("silver");
+	
+	for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore5);
+		array[i].setAttribute("class", "void");
+		
+	}
+}
+
+function deathScore(){
+		alert("YOU HIT ERIN! YOU LOST!");
+		timeleft = 1;
+}
+
+function removeMoles1() {
+	
+
+	array = document.getElementsByClassName("active");
+		for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", addScore1);
+		array[i].setAttribute("class", "void");
+		
+	}
+}
+function removeMoles10() {
+	array = document.getElementsByClassName("gold");
+		for( var i = 0; i < array.length; i++) {
+			//loops through and removes all +1 moles
+		array[i].removeEventListener( "click", addScore10);
+		array[i].setAttribute("class", "void");
+		
+	}
+	}
+function removeMolesDeath() {
+	
+
+	array = document.getElementsByClassName("deathmole");
+	
+	for( var i = 0; i < array.length; i++) {
+		array[i].removeEventListener( "click", deathScore);
+		array[i].setAttribute("class", "void");
+		
+	}
+}
+
+function removeMoles11() {
+	
+//gets array of all +1 moles
+	array = document.getElementsByClassName("active1");
+		for( var i = 0; i < array.length; i++) {
+			//loops through and removes all +1 moles
+		array[i].removeEventListener( "click", addScore11);
+		array[i].setAttribute("class", "void");
+		
+	}
+}
+
+
+		
 function coord(){
 		var randomPlaceX = Math.floor(Math.random()*3)+1;
 		var randomPlaceY = Math.floor(Math.random()*5)+1;
@@ -25,8 +134,8 @@ function coord(){
 
 
 function moveMoleAround11() {
-				var coord1 = coord();
-			if (coord1 == coords){
+			coord1 = coord();
+			if (coord1 === coords){
 				coords = coord();
 				}
 				else{
@@ -42,8 +151,8 @@ function moveMoleAround11() {
 }
 
 function moveMoleAround5() {
-				var coord1 = coord();
-			if (coord1 == coords){
+				coord1 = coord();
+			if (coord1 === coords){
 				coords = coord();
 				}
 				else{
@@ -58,8 +167,8 @@ function moveMoleAround5() {
 }
 
 function moveMoleDeath() {
-				var coord1 = coord();
-			if (coord1 == coords){
+				coord1 = coord();
+			if (coord1 === coords){
 				coords = coord();
 				}
 				else{
@@ -70,14 +179,28 @@ function moveMoleDeath() {
 			document.getElementById(coords).setAttribute("class", "deathmole");
 //adds event listener
 			document.getElementById(coords).addEventListener( "click", deathScore );
-			//removes +1 moles after a second
+			//removes death moles after a second
 			setTimeout(removeMolesDeath, 1000);
 }
-
+function moveMoleAroundGold() {
+			coord1 = coord();
+			if (coord1 === coords){
+				coords = coord();
+				}
+				else{
+			coords = coord1;
+					}
+	//sets class
+			document.getElementById(coords).setAttribute("class", "gold");
+//adds event listener
+			document.getElementById(coords).addEventListener( "click", addScore10 );
+			//removes Gold moles after a 0.75 seconds
+			setTimeout(removeMoles10, 750);
+	}
 
 function moveMoleAround1() {
 			var coord1 = coord();
-			if (coord1 == coords){
+			if (coord1 === coords){
 				coords = coord();
 				}
 				else{
@@ -105,17 +228,19 @@ function start() {
 	inter1 = setInterval(moveMoleAround11,1700);
 	death = setInterval(moveMoleDeath,15000);
 	inter5 = setInterval(moveMoleAround5, 5000);
+	inter10 = setInterval(moveMoleAroundGold,7500);
 	var t=setInterval(countdown_timer, 1000); 
 
 	function countdown_timer()  {
 		document.getElementById("time").innerHTML = timeleft;
 		timeleft--;
-		if(timeleft == -1) {
+		if(timeleft === -1) {
 			total = currentScore + total;
 			clearInterval(t);
 			clearInterval(inter);
 			clearInterval(inter1);
 			clearInterval(inter5);
+			clearInterval(inter10);
 			alert("Time's Up!");
 			document.getElementById("start").disabled = false;
 		//makes HighScore
@@ -130,28 +255,8 @@ function start() {
 	  }
 	}
 
-		var molePicker = 
-		[
-			"mole+1",
-			"mole+1",
-			"mole+1",
-			"mole+1",
-			"mole+1",
-			"mole+0",
-			"mole+0",
-			"mole+0",
-			"mole+0",
-			"mole+0",
-			//"mole+5"  There are 5 +1 to make +1 more common. 
-			];
-			
-		var randomNumber = Math.floor(Math.random()*molePicker.length);
-		var  jack = molePicker[randomNumber];
-	
-		
 
-		//Random Place Generator
-		setTimeout(disableButton,50)
+		setTimeout(disableButton,50);
 } // end start function
 
 function disableButton()
@@ -161,34 +266,8 @@ function disableButton()
 }
 	
 	
-function addScore1(){
-				currentScore++;
-				document.getElementById("score").innerHTML = currentScore;
-				array = document.getElementsByClassName("active");
-		for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", addScore1);
-		array[i].setAttribute("class", "void");
-		
-	}
-			}
-function addScore5(){
-				currentScore = currentScore + 5;
-		
-				document.getElementById("score").innerHTML = currentScore;
-				array = document.getElementsByClassName("silver");
-		for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", addScore5);
-		array[i].setAttribute("class", "void");
-			}
-
-}
-//picks random coord for mole
 
 
-function deathScore(){
-		alert("YOU HIT ERIN! YOU LOST!");
-		timeleft = 1;
-}
 		
 
 		//creates a +5 mole
@@ -196,65 +275,13 @@ function deathScore(){
 //Creates a +1 mole		 
 
 
-function removeMoles5() {
-	
-
-	var array = document.getElementsByClassName("silver");
-	
-	for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", addScore5);
-		array[i].setAttribute("class", "void");
-		
-	}
-}
-
-function removeMoles1() {
-	
-
-	array = document.getElementsByClassName("active");
-		for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", addScore1);
-		array[i].setAttribute("class", "void");
-		
-	}
-}
-
-function removeMolesDeath() {
-	
-
-	array = document.getElementsByClassName("deathmole");
-	
-	for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", deathScore);
-		array[i].setAttribute("class", "void");
-		
-	}
-}
-
-function removeMoles11() {
-	
-//gets array of all +1 moles
-	array = document.getElementsByClassName("active1");
-		for( var i = 0; i < array.length; i++) {
-			//loops through and removes all +1 moles
-		array[i].removeEventListener( "click", addScore11);
-		array[i].setAttribute("class", "void");
-		
-	}
-}
 
 
 
-function addScore11(){
-				currentScore++;
-				document.getElementById("score").innerHTML = currentScore;
-				array = document.getElementsByClassName("active1");
-		for( var i = 0; i < array.length; i++) {
-		array[i].removeEventListener( "click", addScore11);
-		array[i].setAttribute("class", "void");
-		
-	}
-			}
+
+
+
+
 	
 
 
